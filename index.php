7d0d2edb60a12528b7eb983783ec68e3b03d5b1d
SOku <alexandre.pellae@gmail.com>
2019-06-24 06:55:52 +0200

    ajax & json

--- index.php
+++ index.php
@@ -41,10 +41,11 @@
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 		<!-- JS -->
+		<script src="js/ext_script/markerclusterer.js"></script>
     	<script src="js/gmap.js"></script>
         <!-- Gmaps -->
 		<script async defer
-    	src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCtbSOqG5eE8gT3Pwo7D1ClrJi3X97HAi0&callback=initMap">
+    	src="https://maps.googleapis.com/maps/api/js?key=API_KEY&callback=initMap">
    		</script>
     </body>
 </html>
