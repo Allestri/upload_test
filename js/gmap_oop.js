71dbde8937be582a47d019f0a0e2d450bee9cbf1
SOku <alexandre.pellae@gmail.com>
2019-07-02 16:56:11 +0200

    refactorisation gmap oop

--- js/gmap_oop.js
+++ js/gmap_oop.js
@@ -3,26 +3,22 @@
 
 function displayMap()
 {
+	var self = this;
 	var gmap = null;
+	var markers = [];
 	
 	this.initialization = function() {
 		this.initMap();
 		this.refreshMap();
 	}
 	
-	// Google Map
-	this.initMap = function () {
-	    var crozon = {lat: 48.243982, lng: -4.432997};
-	    gmap = new google.maps.Map(document.getElementById('map'), {
-	          center: crozon,
-	          zoom: 5
-	    });
-	    
-	    $.ajax({
+	// Fetch the points
+	this.getPoints = function() {
+		$.ajax({
 	        type: "GET",
 	        url: "markers.json",
 	        dataType: "json",
-	        success: function(data) {
+	        success: (data)=> {
 	            console.log(data[2].lat, data[2].lng);
 	            console.log(data.length);
 	            // Loop through each locations.
@@ -31,37 +27,38 @@ function displayMap()
 	                    position: new google.maps.LatLng(data[i].lat, data[i].lng),
 	                    map: gmap
 	                });
-	            };
+	                markers.push(marker);
+	            }
+	    		// Clusterer
+	    		var markerCluster = new MarkerClusterer(gmap, markers,{
+	    			imagePath: 'images/assets/m'
+	    		});
 	        },
 	        error : function(result, status, error){
 	            $(".container").append("<p>Erreur</p>");
 	        }
 	    });
-	
 	};
 	
+	// Init Google Map
+	this.initMap = function () {
+	    var crozon = {lat: 48.243982, lng: -4.432997};
+	    gmap = new google.maps.Map(document.getElementById('map'), {
+	          center: crozon,
+	          zoom: 5
+	    });
+	    
+	    this.getPoints();
+	
+	};	
+	
+	// Refresh the map (test)
 	this.refreshMap = function () {
-		$("button").click(function(){
-			 $.ajax({
-			        type: "GET",
-			        url: "markers.json",
-			        dataType: "json",
-			        success: function(data) {
-			            // Loop through each locations.
-			            for(var i = 0; i < data.length; i++){
-			                marker = new google.maps.Marker({
-			                    position: new google.maps.LatLng(data[i].lat, data[i].lng),
-			                    map: gmap
-			                });
-			            };
-			        },
-			        error : function(result, status, error){
-			            $(".container").append("<p>Erreur</p>");
-			        }
-			    
-			    });
+		$("button").click(function () {
+			self.getPoints();
 		});
-	};
+	};	
+	
 }
 
 function initApp(){
